9b30786cf89a8f67abebdc38b82ce6c78a9ec3de
Zersiax <florianbeijers@gmail.com>
2017-02-09 20:30:21 +0000

    true initial commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Tvtutor
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+	    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
+  end
+end
