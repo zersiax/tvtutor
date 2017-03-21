91d09547273798309861f076e8ba85c25b337659
Zersiax <florianbeijers@gmail.com>
2017-03-21 23:45:00 +0000

    added validations for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :description, validates: true
+  validates :cost, presence: true, numericality: { greater_then_or_equal_to: 0 }
   
 end
